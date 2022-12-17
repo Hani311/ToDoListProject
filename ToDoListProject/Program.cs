44f6ad8853454f5ff4246f490c4a0598393359a7
Hani <57943681+Hani311@users.noreply.github.com>
2022-12-17 20:50:41 +0100

    Add project files.

---
+++ ToDoListProject/Program.cs
@@ -0,0 +1,142 @@
+﻿ToDoList toDoList = new ToDoList("C:\\VScode repos .net\\ToDoListProject\\ToDoListProject\\todolist.txt"); // select your own path for your file
+
+ bool isCompleted;
+ while (true)
+{
+    Console.WriteLine("1. Add Task");
+    Console.WriteLine("2. Edit Task");
+    Console.WriteLine("3. Delete Task");
+    Console.WriteLine("4. View Tasks");
+    Console.WriteLine("5. Exit");
+    Console.Write("Enter your choice: ");
+
+    int choice = int.Parse(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        List<Task> tasks = toDoList.GetTasks();
+        Console.Write("Enter task name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter a small task description: ");
+        string description = Console.ReadLine();
+        Console.Write("Enter due date: ");
+        DateTime dueDate;
+        string[] formats = { "dd/MM/yyyy", "dd/M/yyyy", "d/M/yyyy", "d/MM/yyyy",
+                 "dd/MM/yy", "dd/M/yy", "d/M/yy", "d/MM/yy", "yy/M/dd","yyyy/M/dd", "yyyy/MM/dd", "yy/MM/dd"};  // allow the user to write the date in different formats
+
+        while (!DateTime.TryParseExact(Console.ReadLine(), formats,
+             System.Globalization.CultureInfo.InvariantCulture,
+             System.Globalization.DateTimeStyles.None,
+             out dueDate))
+        {
+            Console.WriteLine("Your input is incorrect. Please input again.");
+        }
+
+        while (dueDate < DateTime.Now) // if the date is in the past will print a message and will ask to write the date again
+        {
+            Console.WriteLine("The date is in the past please write the date again", Console.ForegroundColor = ConsoleColor.Red);
+            Console.ResetColor();
+            while (!DateTime.TryParseExact(Console.ReadLine(), formats,
+             System.Globalization.CultureInfo.InvariantCulture,
+             System.Globalization.DateTimeStyles.None,
+             out dueDate))
+            {
+                Console.WriteLine("Your input is incorrect. Please input again.", Console.ForegroundColor = ConsoleColor.Red);
+                Console.ResetColor();
+            }
+        }
+        toDoList.AddTask(name, description, dueDate);
+    }
+    else if (choice == 2)
+    {
+        List<Task> tasks = toDoList.GetTasks();
+        Console.WriteLine("your list of tasks");
+        int indexx = 0;
+        Console.WriteLine("index".PadRight(15) + "Name".PadRight(15) + "Description".PadRight(50) + "DueDate".PadRight(35) + "Status".PadRight(15));
+        foreach (Task task in tasks)
+        {
+            indexx = indexx + 1;
+
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(indexx.ToString().PadRight(15) + task.Name.PadRight(15) + task.Description.PadRight(50) + task.DueDate.ToString("yyyy-MM-dd").PadRight(35) + task.IsCompleted.ToString().PadRight(15));
+            Console.ResetColor();
+        }
+        Console.WriteLine("------------------------------------------------------------------");
+
+        Console.Write("Enter the \u001b[31mTASK NUMBER\u001b[0m to edit ");
+        int index = int.Parse(Console.ReadLine());
+        Console.Write("Enter task name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter task description: ");
+        string description = Console.ReadLine();
+        Console.Write("Enter due date: ");
+        DateTime dueDate;
+        string[] formats = { "dd/MM/yyyy", "dd/M/yyyy", "d/M/yyyy", "d/MM/yyyy",
+                 "dd/MM/yy", "dd/M/yy", "d/M/yy", "d/MM/yy", "yy/M/dd","yyyy/M/dd", "yyyy/MM/dd", "yy/MM/dd"};  // allow the user to write the date in different formats
+
+        while (!DateTime.TryParseExact(Console.ReadLine(), formats,
+             System.Globalization.CultureInfo.InvariantCulture,
+             System.Globalization.DateTimeStyles.None,
+             out dueDate))
+        {
+            Console.WriteLine("Your input is incorrect. Please input again.");
+        }
+
+        while (dueDate < DateTime.Now) // if the date is in the past will print a message and will ask to write the date again
+        {
+            Console.WriteLine("The date is in the past please write the date again", Console.ForegroundColor = ConsoleColor.Red);
+            Console.ResetColor();
+            while (!DateTime.TryParseExact(Console.ReadLine(), formats,
+             System.Globalization.CultureInfo.InvariantCulture,
+             System.Globalization.DateTimeStyles.None,
+             out dueDate))
+            {
+                Console.WriteLine("Your input is incorrect. Please input again.", Console.ForegroundColor = ConsoleColor.Red);
+                Console.ResetColor();
+
+            }
+        }
+        Console.Write("Is completed? (true/false): ");
+        isCompleted = bool.Parse(Console.ReadLine());
+        toDoList.EditTask(index, name, description, dueDate, isCompleted);
+    }
+    else if (choice == 3)
+    {
+        List<Task> tasks = toDoList.GetTasks();
+        foreach (Task task in tasks)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(task.Name.PadRight(15) + task.Description.PadRight(15) + task.DueDate.ToString("yyyy-MM-dd").PadRight(35) + task.IsCompleted.ToString().PadRight(15));
+            Console.ResetColor();
+
+        }
+        Console.WriteLine("------------------------------------------------------------------");
+
+        Console.Write("Enter \u001b[31mTASK NAME\u001b[0m to delete it form your list: ");
+        string name = Console.ReadLine();
+
+        toDoList.DeleteTask(name);
+    }
+    else if (choice == 4)
+    {
+        List<Task> tasks = toDoList.GetTasks();
+        List<Task> sorted = tasks.OrderBy(o => o.DueDate).ToList();
+        Console.WriteLine("your list of tasks");
+        Console.WriteLine("Name".PadRight(15) + "Description".PadRight(50) + "DueDate".PadRight(35) + "Status".PadRight(15));
+        foreach (Task task in sorted)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(task.Name.PadRight(15) + task.Description.PadRight(50) + task.DueDate.ToString("yyyy-MM-dd").PadRight(35) + task.IsCompleted.ToString().PadRight(15));
+            Console.ResetColor();
+
+        }
+        Console.WriteLine("------------------------------------------------------------------");
+
+    }
+    else if (choice == 5)
+    {
+        break;
+    }
+}
+ 
---
+++ ToDoListProject/ToDoList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class ToDoList
+    {
+        private List<Task> tasks;
+        string filePath = "C:\\VScode repos .net\\ToDoListProject\\ToDoListProject\\todolist.txt"; //create the file path  make sure to select your own path for your file
+
+
+        public ToDoList(string filePath)
+        {
+            this.filePath = filePath;
+            tasks = new List<Task>();
+            LoadTasksFromFile();
+        }
+
+        private void LoadTasksFromFile()
+        {
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        string[] parts = line.Split(',');
+                        Task task = new Task
+                        {
+
+                            Name = parts[0],
+                            Description = parts[1],
+                            DueDate = DateTime.Parse(parts[2]),
+                            IsCompleted = bool.Parse(parts[3])
+                        };
+                        tasks.Add(task);
+                    }
+                }
+            }
+        }
+
+        private void SaveTasksToFile()
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (Task task in tasks)
+                {
+                    string line = $"{task.Name},{task.Description},{task.DueDate:yyyy-MM-dd},{task.IsCompleted}";
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        public void AddTask(string name, string description, DateTime dueDate)
+        {
+            Task task = new Task
+            {
+                // Index = index,
+                Name = name,
+                Description = description,
+                DueDate = dueDate,
+                IsCompleted = false
+            };
+            tasks.Add(task);
+            SaveTasksToFile();
+        }
+
+        public void EditTask(int index, string name, string description, DateTime dueDate, bool isCompleted)
+        {
+            Task task = tasks[index - 1];
+            task.Name = name;
+            task.Description = description;
+            task.DueDate = dueDate;
+            task.IsCompleted = isCompleted;
+            SaveTasksToFile();
+        }
+
+        public void DeleteTask(string name)
+        {
+            Task task = tasks.Find(t => t.Name == name);
+            tasks.Remove(task);
+            SaveTasksToFile();
+        }
+
+        public List<Task> GetTasks()
+        {
+            return tasks;
+        }
+
+
+    }
